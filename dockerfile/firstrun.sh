2d988431d2a83d2f8ff3004642ac19ec2f4739ab
lannerkr <lannerkr@gmail.com>
2023-11-06 01:09:47 +0000

    dockerfile added

---
+++ dockerfile/firstrun.sh
@@ -0,0 +1,5 @@
+ln -s /usr/local/src/assets/config config
+ln -s /usr/local/src/assets/template template
+ln -s /usr/local/src/assets/tls tls
+ln -s /usr/local/src/assets/user.db user.db
+ln -s /usr/local/src/witty witty
